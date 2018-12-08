cf8a8e259b4f86ab01f5904d5a32625a6ce1c7ad
Akshat <33050725+aaaakshat@users.noreply.github.com>
2018-12-08 11:33:15 +0800

    Updated account handling and transactions

--- src/Transactions.java
+++ src/Transactions.java
@@ -1,9 +1,25 @@
 
 public class Transactions {
 
+	String transactionMsg;
+	Double transactionAmount;
+	Double newAccountBalance;
+	String history;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
+	public void addTransactionObj(String transactionMsg, Double transactionAmount, Double newAccountBalance) {
+		this.transactionMsg = transactionMsg;
+		this.transactionAmount = transactionAmount;
+		this.newAccountBalance = newAccountBalance;
+		String history = transactionMsg + Double.toString(transactionAmount) + Double.toString(newAccountBalance);
+		
+	}
+	
+	public void printHistory() {
+		System.out.println();
+	}
 }
